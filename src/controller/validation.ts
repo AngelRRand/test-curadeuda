d6dac1184f6411871e47c8bc48753b3af4b0b49d
Horacio Rodriguez <96556399+AngelRRand@users.noreply.github.com>
2025-02-08 22:22:30 -0300

    feat: start login and register screens development

---
+++ src/controller/validation.ts
@@ -0,0 +1,15 @@
+import data from "@/data/users.json";
+
+export const isValidEmail = (email: string) => {
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+	return emailRegex.test(email)
+}
+
+export const isEmailTaken = (email: string) => {
+	return data.some((user) => user.email.toLowerCase() === email.toLowerCase())
+}
+
+export const isValidPassword = (password: string) => {
+	return password.length >= 5
+}
+
